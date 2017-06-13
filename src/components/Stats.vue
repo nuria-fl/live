704845a72cd9cff5b9bc0c69537ddf1e57083549
Nuria <soriano.nuria@gmail.com>
2017-06-13 19:10:10 +0200

    fix(stats): reset countdown before destroy

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -11,6 +11,11 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'stats',
+  data() {
+    return {
+      loop: null
+    }
+  },
   computed: {
     ...mapState(['stats', 'gameOver'])
   },
@@ -18,16 +23,23 @@ export default {
     countdown() {
       const minute = 10 * 1000
       if(this.$store.state.gameOver === false){
-        setInterval(() => {
+        this.loop = setInterval(() => {
           this.$store.commit('decrease', { stat: 'water', amount: 3 })
           this.$store.commit('decrease', { stat: 'food', amount: 2 })
           this.$store.commit('decrease', { stat: 'sleep', amount: 1 })
         }, minute)
       }
+    },
+    resetCountdown(){
+      clearInterval(this.loop)
+      this.loop = null
     }
   },
   mounted(){
     this.countdown()
+  },
+  beforeDestroy(){
+    this.resetCountdown()
   }
 }
 </script>
